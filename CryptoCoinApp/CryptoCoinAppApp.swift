e94dcb18595bb2887242cfa1c792d19482cefada
maged mohamed <maged12345@Github.com>
2023-09-30 22:27:04 +0300

    Initial Commit

---
+++ CryptoCoinApp/CryptoCoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinAppApp.swift
+//  CryptoCoinApp
+//
+//  Created by s on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
