b714d5446d084e25cb06c1c138166f3e7add2610
maged mohamed <maged12345@Github.com>
2023-10-05 01:02:01 +0300

    filter

--- CryptoCoinApp/Home/ViewModels/HomeViewModel.swift
+++ CryptoCoinApp/Home/ViewModels/HomeViewModel.swift
@@ -13,18 +13,31 @@ class HomeViewModel: ObservableObject{
     @Published  var searchText = ""
     let networkService = CoinsManager()
     
-    var anycancellables =  Set<AnyCancellable>()
-
+    var cancellables =  Set<AnyCancellable>()
+    
     init () {
         getCoins()
     }
     
     func getCoins() {
         networkService.$allCoins
-            .sink { [weak self] returnedData in
-                self?.allCoins = returnedData
+            .combineLatest($searchText)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map({ coins, text -> [CoinModel] in
+                if text.isEmpty {
+                    return coins
+                }else {
+                    let searchTextLowercased = text.lowercased()
+                    return coins.filter { coin in
+                        coin.id.lowercased().contains(searchTextLowercased) || coin.name.contains(searchTextLowercased)
+                    }
+                }
+                
+            })
+            .sink { [weak self] filteredValue in
+                self?.allCoins = filteredValue
             }
-            .store(in: &anycancellables)
+            .store(in: &cancellables)
     }
     
     
